1cd893d60619f4b33fd0ba6074962371743f17a0
Olympicson <olympicson@DESKTOP-HKJ1ATA.localdomain>
2022-10-17 04:31:23 -0700

    Update one 17th

---
+++ practice2/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void print_num(int n);
+
+
+#endif /* _MAIN_H_ */
