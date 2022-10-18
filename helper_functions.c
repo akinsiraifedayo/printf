f0010002d46ca00077e732be159667ce0751a466
ammyee <okoyeamarachukwu13@gmail.com>
2022-10-18 11:30:09 -0700

    Prints a string in reverse

--- helper_functions.c
+++ helper_functions.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * rev_string - reverses a string in place
+ * rev_string - reverses a string or group of strings in a place
  *
  * @s: string to reverse
  * Return: A pointer to a character
