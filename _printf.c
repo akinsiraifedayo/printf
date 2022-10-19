e4a9abe44216bbbc043557b7e79841c27847142b
ammyee <okoyeamarachukwu13@gmail.com>
2022-10-18 22:00:28 -0700

    Arguements

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
+ * _printf - Receives the main string and all the necessary parameters needed to
  * print a formated string
  * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
